448b4a776f973570ebfe313e435bd95f63341960
ab17005-collab <ab17005@ues.edu.sv>
2025-10-27 15:45:15 -0600

    Codigo barra de busqueda y matriz tipo List agregados

--- Clave1_Grupo1/Form2.Designer.cs
+++ Clave1_Grupo1/Form2.Designer.cs
@@ -29,22 +29,29 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtBarraBusqueda = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.btnBuscar = new System.Windows.Forms.Button();
-            this.label3 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.btnCrearUsuario = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.chkTodos = new System.Windows.Forms.CheckBox();
+            this.lblUsuarioNoEncontrado = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // textBox1
+            // txtBarraBusqueda
             // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(191, 184);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(382, 32);
-            this.textBox1.TabIndex = 0;
+            this.txtBarraBusqueda.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBarraBusqueda.Location = new System.Drawing.Point(191, 184);
+            this.txtBarraBusqueda.Name = "txtBarraBusqueda";
+            this.txtBarraBusqueda.Size = new System.Drawing.Size(382, 32);
+            this.txtBarraBusqueda.TabIndex = 0;
             // 
             // label1
             // 
@@ -68,16 +75,7 @@
             this.btnBuscar.TabIndex = 2;
             this.btnBuscar.Text = "Buscar";
             this.btnBuscar.UseVisualStyleBackColor = false;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.label3.Location = new System.Drawing.Point(278, 228);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(211, 13);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "Numero de cuenta / DUI /Telefono / Email";
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
             // 
             // pictureBox1
             // 
@@ -99,19 +97,108 @@
             this.linkLabel1.TabStop = true;
             this.linkLabel1.Text = "Cerrar sesion";
             // 
+            // btnCrearUsuario
+            // 
+            this.btnCrearUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCrearUsuario.Location = new System.Drawing.Point(191, 38);
+            this.btnCrearUsuario.Name = "btnCrearUsuario";
+            this.btnCrearUsuario.Size = new System.Drawing.Size(93, 23);
+            this.btnCrearUsuario.TabIndex = 7;
+            this.btnCrearUsuario.Text = "Crear usuario";
+            this.btnCrearUsuario.UseVisualStyleBackColor = true;
+            this.btnCrearUsuario.Click += new System.EventHandler(this.btnCrearUsuario_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(178, 259);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Filtros:";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(283, 259);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(114, 17);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "Numero de cuenta";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(403, 258);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(89, 17);
+            this.checkBox2.TabIndex = 10;
+            this.checkBox2.Text = "Identificacion";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(498, 259);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(68, 17);
+            this.checkBox3.TabIndex = 11;
+            this.checkBox3.Text = "Telefono";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(572, 259);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(57, 17);
+            this.checkBox4.TabIndex = 12;
+            this.checkBox4.Text = "Correo";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // chkTodos
+            // 
+            this.chkTodos.AutoSize = true;
+            this.chkTodos.Location = new System.Drawing.Point(221, 259);
+            this.chkTodos.Name = "chkTodos";
+            this.chkTodos.Size = new System.Drawing.Size(56, 17);
+            this.chkTodos.TabIndex = 13;
+            this.chkTodos.Text = "Todos";
+            this.chkTodos.UseVisualStyleBackColor = true;
+            // 
+            // lblUsuarioNoEncontrado
+            // 
+            this.lblUsuarioNoEncontrado.AutoSize = true;
+            this.lblUsuarioNoEncontrado.ForeColor = System.Drawing.Color.Red;
+            this.lblUsuarioNoEncontrado.Location = new System.Drawing.Point(229, 219);
+            this.lblUsuarioNoEncontrado.Name = "lblUsuarioNoEncontrado";
+            this.lblUsuarioNoEncontrado.Size = new System.Drawing.Size(308, 13);
+            this.lblUsuarioNoEncontrado.TabIndex = 14;
+            this.lblUsuarioNoEncontrado.Text = "Usuario no encontraco, por favor verifique los datos ingresados.";
+            this.lblUsuarioNoEncontrado.Visible = false;
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblUsuarioNoEncontrado);
+            this.Controls.Add(this.chkTodos);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnCrearUsuario);
             this.Controls.Add(this.linkLabel1);
-            this.Controls.Add(this.label3);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.btnBuscar);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtBarraBusqueda);
             this.Name = "Form2";
             this.ShowIcon = false;
+            this.Load += new System.EventHandler(this.Form2_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -120,11 +207,18 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtBarraBusqueda;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnBuscar;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Button btnCrearUsuario;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox chkTodos;
+        private System.Windows.Forms.Label lblUsuarioNoEncontrado;
     }
 }
--- Clave1_Grupo1/Form2.cs
+++ Clave1_Grupo1/Form2.cs
@@ -16,5 +16,60 @@ namespace Clave1_Grupo1
         {
             InitializeComponent();
         }
+
+        // Definir varables globales
+        List<List<dynamic>> usuarios = new List<List<dynamic>>();
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+        // Agregar algunos usuarios de ejemplo
+        usuarios.Add(new List<dynamic> { "2205457100", "Jaime Roberto", "Aldana Beltran", "29", "72399078", "ab17005@ues.edu.sv", "4440 Caynor Circle, Rochelle Park, NJ 07662", "055613056" });
+        usuarios.Add(new List<dynamic> { "2205457111", "Maria Fernanda", "Lopez Martinez", "25", "72399079", "NazarOlveraSuarez@superrito.com", "1234 Elm Street, Springfield, IL 62704", "055613057" });
+
+        // Configurar layout del formulario
+        chkTodos.Checked = true;
+        lblUsuarioNoEncontrado.Visible = false;
+        }
+
+        private void btnCrearUsuario_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            string entrada = txtBarraBusqueda.Text.ToLower().Trim();
+            bool usuarioEncontrado = false;
+
+            // Encontrar usuario
+            foreach (var usuario in usuarios)
+            {
+                if (usuario[0].ToString().ToLower() == entrada ||
+                    usuario[4].ToString().ToLower() == entrada ||
+                    usuario[5].ToString().ToLower() == entrada ||
+                    usuario[7].ToString().ToLower() == entrada)
+                {
+                    usuarioEncontrado = true;
+                    lblUsuarioNoEncontrado.Visible = false;
+                    // Mostrar datos del usuario encontrado
+                    MessageBox.Show(
+                        $"Usuario encontrado:\n\n" +
+                                        $"ID: {usuario[0]}\n" +
+                                        $"Nombre: {usuario[1]} {usuario[2]}\n" +
+                                        $"Edad: {usuario[3]}\n" +
+                                        $"Teléfono: {usuario[4]}\n" +
+                                        $"Email: {usuario[5]}\n" +
+                                        $"Dirección: {usuario[6]}\n" +
+                                        $"Código de Cliente: {usuario[7]}"
+                        );
+                }
+
+                // Si no se encuentra el usuario
+                if (!usuarioEncontrado)
+                {
+                    lblUsuarioNoEncontrado.Visible = true;
+                }
+            }
+        }
     }
 }
