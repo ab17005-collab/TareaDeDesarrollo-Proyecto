eef947446e9532fb80ec1d939cd72c2a9de2254e
ab17005-collab <ab17005@ues.edu.sv>
2025-11-15 01:10:59 -0600

    borde de panel corregido

--- Clave1_Grupo1/Formularios/Form4.Designer.cs
+++ Clave1_Grupo1/Formularios/Form4.Designer.cs
@@ -105,6 +105,7 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Controls.Add(this.lblEdad);
             this.panel1.Controls.Add(this.lblEspecie);
